4f7c8441d7ebc4c4e921cca2b62e1430d9412669
Mehedix-2 <136720187+Mehedix-2@users.noreply.github.com>
2025-07-11 08:15:53 +0600

    Update sonic.py

--- sonic.py
+++ sonic.py
@@ -7,9 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-import random
+
 
 
 # Suppress unnecessary logging
